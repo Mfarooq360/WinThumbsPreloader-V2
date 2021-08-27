d1df3f5193ab094dc177f0708e595583e6a9f361
inthebrilliantblue <intheprettypink@gmail.com>
2021-08-27 17:40:21 -0500

    Forced multithreading code

--- WinThumbsPreloader/ConsoleWTP/Program.cs
+++ WinThumbsPreloader/ConsoleWTP/Program.cs
@@ -14,39 +14,18 @@ namespace ConsoleWTP
             if (string.IsNullOrWhiteSpace(args[0])) {
                 Console.WriteLine("Please provide a location to scan!");
             }
-            //Set multithread
-            bool multithread = true;
-            if (!string.IsNullOrWhiteSpace(args[1])) {
-                if (args[1] == "false") {
-                    multithread = false;
-                }
-            }
             //Get the items first before doing work
             List<string> items = DirectoryScanner.GetItemsBulk(args[0], true);
-            if (!multithread)
-            {
-                ThumbnailPreloader thumbnailPreloader = new ThumbnailPreloader();
-                int count = items.Count();
-                int i = 0;
-                foreach (string item in items)
+            Parallel.ForEach(
+                items,
+                new ParallelOptions { MaxDegreeOfParallelism = 2048 },
+                item =>
                 {
-                    i++;
-                    Console.WriteLine(i+"/"+count+": "+item);
+                    ThumbnailPreloader thumbnailPreloader = new ThumbnailPreloader();
+                    Console.WriteLine(item);
                     thumbnailPreloader.PreloadThumbnail(item);
                 }
-            }
-            else
-            {
-                Parallel.ForEach(
-                    items,
-                    new ParallelOptions { MaxDegreeOfParallelism = 2048 },
-                    item =>
-                    {
-                        ThumbnailPreloader thumbnailPreloader = new ThumbnailPreloader();
-                        Console.WriteLine(item);
-                        thumbnailPreloader.PreloadThumbnail(item);
-                    });
-            }
+            );
         }
     }
 }
